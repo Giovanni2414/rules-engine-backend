858d14e870bdf096dec483708f69982a098b8afe
Giovanni Mosquera <Giovanni2414g@gmail.com>
2022-12-15 00:16:39 -0500

    Created branch to test CI/CD with AWS

---
+++ rules-engine/src/main/java/com/perficient/rulesengine/api/AWSTestAPI.java
@@ -0,0 +1,17 @@
+package com.perficient.rulesengine.api;
+
+import com.perficient.rulesengine.dto.ColumnDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@RequestMapping("/aws")
+public interface DBMetadataAPI {
+
+    @GetMapping
+    public ResponseEntity<String> health();
+
+}
---
+++ rules-engine/src/main/java/com/perficient/rulesengine/controller/AWSTestController.java
@@ -0,0 +1,21 @@
+package com.perficient.rulesengine.controller;
+
+import com.perficient.rulesengine.api.*;
+import com.perficient.rulesengine.service.DBMetadataService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@AllArgsConstructor
+public class DBMetadataController implements AWSTestAPI {
+
+    @Override
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Spring boot application running on EC2 instance from AWS!!!")
+    }
+
+}
