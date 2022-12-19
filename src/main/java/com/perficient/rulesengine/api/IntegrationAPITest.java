2d72abb542da7c1b0dff91353f80c227d127ba77
Giovanni Mosquera <Giovanni2414g@gmail.com>
2022-12-19 14:05:45 -0500

    Changed password db 2

--- src/main/java/com/perficient/rulesengine/api/IntegrationAPITest.java
+++ src/main/java/com/perficient/rulesengine/api/IntegrationAPITest.java
@@ -3,7 +3,7 @@ package com.perficient.rulesengine.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("CICD")
+@RequestMapping("/CICD")
 public interface IntegrationAPITest {
     @GetMapping
     public String testCICD();
